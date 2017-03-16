e6eb0e0e336ceaa6b5e00eef33b099313f1a509a
aldonin_k <aldonin.k@gmail.com>
2017-03-16 10:23:51 +0300

    Add game prototype

---
+++ script.js
@@ -0,0 +1,24 @@
+$(".go").click(function() {
+	var players = [
+		$("input[name=player1]").val(),
+		$("input[name=player2]").val(),
+		$("input[name=player3]").val(),
+		$("input[name=player4]").val()
+	];
+
+	$(".board").hide();
+	$.each(players, function(index, value) {
+		$("#" + index).html(value);
+	});
+	$(".game").show();
+});
+
+$(".minus").click(function() {
+	var value = $(this).next();
+	value.html(value.html() - 1);
+});
+
+$(".plus").click(function() {
+	var value = $(this).prev();
+	value.html(1*value.html() + 1);
+});
